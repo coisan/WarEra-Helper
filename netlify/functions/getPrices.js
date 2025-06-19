9eed2cedb3a747451b6d801854bf1e0691088aec
coisan <30983818+coisan@users.noreply.github.com>
2025-06-19 14:56:27 +0300

    Update getPrices.js

--- netlify/functions/getPrices.js
+++ netlify/functions/getPrices.js
@@ -1,27 +1,30 @@
-const fetch = require("node-fetch");
-
 exports.handler = async function (event, context) {
-  const proxyUrl = "https://corsproxy.io/?" +
-    encodeURIComponent("https://api2.warera.io/trpc/itemTrading.getPrices");
-
-  const res = await fetch(proxyUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "trpc-batch-mode": "true"
-    },
-    body: JSON.stringify({
-      "0": {
-        "json": {
-          "language": "en"
+  try {
+    const res = await fetch('https://api2.warera.io/trpc/itemTrading.getPrices', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'trpc-batch-mode': 'true'
+      },
+      body: JSON.stringify({
+        "0": {
+          "json": {
+            "language": "en"
+          }
         }
-      }
-    })
-  });
+      })
+    });
 
-  const data = await res.json();
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data)
-  };
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.result.data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ errorType: error.name, errorMessage: error.message })
+    };
+  }
 };
