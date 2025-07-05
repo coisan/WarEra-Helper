de865990d3c26e5d615fb4001cbc813448353ae8
coisan <30983818+coisan@users.noreply.github.com>
2025-07-05 20:07:50 +0300

    Update war_stats.js

--- assets/war_stats.js
+++ assets/war_stats.js
@@ -1,1 +1,8 @@
-
+const path = window.location.pathname;
+if (path === '/' || path.includes('calc.html')) {
+document.getElementById('nav-calc').style.fontWeight = 'bold';
+document.getElementById('nav-calc').style.backgroundColor = '#ddd';
+} else if (path.includes('war_stats.html')) {
+document.getElementById('nav-stats').style.fontWeight = 'bold';
+document.getElementById('nav-stats').style.backgroundColor = '#ddd';
+}
