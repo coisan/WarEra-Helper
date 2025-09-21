48609208103f370d353e81235cc63cdc0c01dd09
coisan <30983818+coisan@users.noreply.github.com>
2025-09-21 14:56:18 +0300

    Update country_wars.js

--- assets/country_wars.js
+++ assets/country_wars.js
@@ -103,7 +103,7 @@ async function buildStats(selectedCountryId) {
     await fetchCountryName(selectedCountryId);
   
     const countDisplay = document.getElementById("battleCount");
-    countDisplay.textContent = `Attacking battles: ${asAttacker}\nDefending battles: ${asDefender}`;
+    countDisplay.innerHTML = `Attacking battles: ${asAttacker}<br>Defending battles: ${asDefender}`;
   
     for (const battle of filteredBattles) {
       const battleId = battle._id;
