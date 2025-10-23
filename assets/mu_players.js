04764fc10c0c5a54502b2fa34011ba9ff1eb2a44
coisan <30983818+coisan@users.noreply.github.com>
2025-10-23 20:28:12 +0300

    Correct damage calculation logic in calcDamage
    Fix calculation of total health and attacks in calcDamage function.

--- assets/mu_players.js
+++ assets/mu_players.js
@@ -52,8 +52,8 @@ function checkBuff(userData) {
 
 function calcDamage(userData) {
   const regenSelect = document.getElementById("regenSelect")
-  const total_health = Math.floor(userData.skills.health.currentBarValue + userData.skills.hunger.currentBarValue * regenSelect.value);
-  const attacks = Math.floor(Math.floor(total_health / (10 * (1 - userData.skills.armor.total/100))) / (1 - userData.skills.dodge.total/100));
+  const total_health = userData.skills.health.currentBarValue + Math.floor(userData.skills.hunger.currentBarValue) * regenSelect.value;
+  const attacks = Math.floor(Math.floor(total_health / (10 * (1 - userData.skills.armor.total/100))) * (1 + userData.skills.dodge.total/100));
   const miss_damage = Math.round(userData.skills.attack.total * (1 - userData.skills.precision.total/100));
   const normal_damage = Math.round(userData.skills.attack.total * (userData.skills.precision.total/100) * (1 - userData.skills.criticalChance.total/100));
   const crit_damage = Math.round(userData.skills.attack.total * (userData.skills.precision.total/100) * (userData.skills.criticalChance.total/100) * (1 + userData.skills.criticalDamages.total/100));
