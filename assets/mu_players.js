41c6f5aa7247bbd9879c503bfe9a6bb14ad84317
coisan <30983818+coisan@users.noreply.github.com>
2025-10-23 20:44:10 +0300

    Update mu_players.js

--- assets/mu_players.js
+++ assets/mu_players.js
@@ -52,7 +52,7 @@ function checkBuff(userData) {
 
 function calcDamage(userData) {
   const regenSelect = document.getElementById("regenSelect")
-  const total_health = Math.max(0, userData.skills.health.currentBarValue) + Math.floor(Math.max(userData.skills.hunger.currentBarValue)) * regenSelect.value;
+  const total_health = Math.max(0, userData.skills.health.currentBarValue) + Math.floor(Math.max(0, userData.skills.hunger.currentBarValue)) * regenSelect.value;
   const attacks = Math.floor(Math.floor(total_health / (10 * (1 - userData.skills.armor.total/100))) * (1 + userData.skills.dodge.total/100));
   const miss_damage = Math.round(userData.skills.attack.total * (1 - userData.skills.precision.total/100));
   const normal_damage = Math.round(userData.skills.attack.total * (userData.skills.precision.total/100) * (1 - userData.skills.criticalChance.total/100));
